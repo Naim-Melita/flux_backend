919617d742cca64c40206297eab25b15ca6f8a0f
nachito02 <arguellojuan08@gmail.com>
2026-03-03 10:56:15 -0300

    postgres database

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,10 +1,22 @@
 import { PrismaClient } from "@prisma/client";
+import { PrismaPg } from "@prisma/adapter-pg";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const globalForPrisma = globalThis as unknown as { prisma?: PrismaClient };
+const connectionString = process.env.DATABASE_URL;
+
+if (!connectionString) {
+  throw new Error("DATABASE_URL no configurada");
+}
+
+const adapter = new PrismaPg({ connectionString });
 
 export const prisma =
   globalForPrisma.prisma ??
   new PrismaClient({
+    adapter,
     log: ["error", "warn"],
   });
 
